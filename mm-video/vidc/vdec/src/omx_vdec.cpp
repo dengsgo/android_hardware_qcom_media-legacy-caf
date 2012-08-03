8437aeb6bb53c8b17ee50fb6f93d5cf9756fe7ff
Shalaj Jain <shalajj@codeaurora.org>
2012-08-03 16:54:09 -0700

    mm-video: vdec: Notify display on interlace video playback
    - Call setMetaData API on each buffer handle in the begnining of
      playback, when the (use)buffers are being registered with the
      component
    
    Change-Id: Ibebd25716a4a744b054eb2c3dfc363e62ca412a0
    (cherry picked from commit 48af9a4245704f77cea9508996bef1f15973af73)

--- mm-video/vidc/vdec/src/omx_vdec.cpp
+++ mm-video/vidc/vdec/src/omx_vdec.cpp
@@ -65,6 +65,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #if defined (_ANDROID_ICS_)
 #include <genlock.h>
+#include <qdMetaData.h>
 #endif
 
 #ifdef _ANDROID_
@@ -4049,6 +4050,12 @@ OMX_ERRORTYPE  omx_vdec::use_output_buffer(
         drv_ctx.ptr_outputbuffer[i].bufferaddr = buff;
         drv_ctx.ptr_outputbuffer[i].mmaped_size =
             drv_ctx.ptr_outputbuffer[i].buffer_len = drv_ctx.op_buf.buffer_size;
+
+#if defined(_ANDROID_ICS_)
+        if (drv_ctx.interlace != VDEC_InterlaceFrameProgressive) {
+            setMetaData(handle, PP_PARAM_INTERLACED, 1);
+        }
+#endif
     } else
 #endif
 
