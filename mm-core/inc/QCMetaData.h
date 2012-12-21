09c148355bd709be9f21c816c2db87841af4eb37
Ajit Khare <ajitk@codeaurora.org>
2012-12-21 11:57:15 -0800

    mm-core: Add kKeySmoothStreaming
    Change-Id: I93036781cab1d81d8e22a626d61348b71203380b
    
    Conflicts:
    	mm-core/inc/QCMetaData.h

--- mm-core/inc/QCMetaData.h
+++ mm-core/inc/QCMetaData.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -62,6 +62,7 @@ enum {
 
     //Extractor sets this
     kKeyUseArbitraryMode     = 'ArbM',  //bool (int32_t)
+    kKeySmoothStreaming      = 'ESmS',  //bool (int32_t)
 
     kKeyMinBlkSize           = 'mibs', //int32_t
     kKeyMaxBlkSize           = 'mabs', //int32_t
