CyanogenMod/android_hardware_qcom_media

mm-core: Add support for Smooth streaming for secure mode playback.
   - This change will add the extension index required for
   - Smooth Streaming for both secure and non secure mode video playback.

Change-Id: I351b5c68702fc2dd83d50801b9bee7f8744c31b6
CRs-Fixed: 451786

mm-core/inc/OMX_QCOMExtns.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2009-2012, The Linux Foundation. All rights reserved.
+Copyright (c) 2009-2013, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -359,6 +359,9 @@ enum OMX_QCOM_EXTN_INDEXTYPE
     OMX_QcomIndexParamSequenceHeaderWithIDR = 0x7F000020,
 
     OMX_QcomIndexEnableExtnUserData = 0x7F000021,
+
+    /*"OMX.QCOM.index.param.video.EnableSmoothStreaming"*/
+    OMX_QcomIndexParamEnableSmoothStreaming = 0x7F000022,
 };
 
 /**

dengsgo/android_hardware_qcom_media-legacy-caf

Conflicts:
	mm-core/inc/OMX_QCOMExtns.h

mm-core/inc/OMX_QCOMExtns.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2009-2012, Code Aurora Forum. All rights reserved.
+Copyright (c) 2009-2013, Code Aurora Forum. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -357,6 +357,9 @@ enum OMX_QCOM_EXTN_INDEXTYPE
     OMX_QcomIndexParamVideoMaxAllowedBitrateCheck = 0x7F00001E,
 
     OMX_QcomIndexEnableSliceDeliveryMode = 0x7F00001F,
+
+    /*"OMX.QCOM.index.param.video.EnableSmoothStreaming"*/
+    OMX_QcomIndexParamEnableSmoothStreaming = 0x7F000022,
 };
 
 /**